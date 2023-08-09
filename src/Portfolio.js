f032be5a92ba2d4598e618c17d52f4a2c7ba8137
retnodamayanti <mayahere5@gmail.com>
2023-08-09 15:39:05 +0930

    added overlay to the portfolio and more projects

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,32 +1,77 @@
-// src/Portfolio.js
 import React from 'react';
 import Project from './components/Project'; 
 import projectInventoryImage from './images/project_inventory.jpg';
+import techBlogImage from './images/online-blog.jpg';
+import weatherDashImage from './images/weather-dashboard.jpg';
+import quizImage from './images/coding_quiz.png';
+import passwordImage from './images/generate-password.jpg';
+import easyShopImage from './images/easy_shop.jpg'
+
+// Import more project images as needed
 
 const Portfolio = () => {
   const projects = [
     {
-      title: 'Project 1',
+      title: 'Project Inventory',
       imageSrc: projectInventoryImage,
       liveLink: 'https://project-inventory-ff35c68cb7af.herokuapp.com/',
       repoLink: 'https://github.com/retnodamayanti/Project_Inventory',
     },
-    // Add more projects here
+    {
+      title: 'Tech Blog', 
+      imageSrc: techBlogImage, 
+      liveLink: 'https://maya-tech-blog-8a50da98b642.herokuapp.com/',
+      repoLink: 'https://github.com/retnodamayanti/tech-blog',
+    },
+    {
+      title: 'Weather Dashboard', 
+      imageSrc: weatherDashImage, 
+      liveLink: 'https://retnodamayanti.github.io/weather-dashboard/',
+      repoLink: 'https://github.com/retnodamayanti/weather-dashboard',
+    },
+    {
+      title: 'Coding Quiz',
+      imageSrc: quizImage,
+      liveLink: 'https://retnodamayanti.github.io/quiz_javascript/index.html',
+      repoLink: 'https://github.com/retnodamayanti/quiz_javascript',
+    },
+    {
+      title: 'Generate Password', 
+      imageSrc: passwordImage, 
+      liveLink: 'https://github.com/retnodamayanti/generate_password_javascript',
+      repoLink: 'https://retnodamayanti.github.io/generate_password_javascript/',
+    },
+    {
+      title: 'Eazy Shopping', 
+      imageSrc: easyShopImage, 
+      liveLink: 'https://davidhccnguyen.github.io/EasierShopping',
+      repoLink: 'https://github.com/DavidHCCNguyen/C07-EazyShopping',
+    },
+   
+    
   ];
 
   return (
-    <section className="portfolio">
-      {projects.map((project, index) => (
-        <Project
-          key={index}
-          title={project.title}
-          imageSrc={project.imageSrc}
-          liveLink={project.liveLink}
-          repoLink={project.repoLink}
-        />
-      ))}
+    <section className="portfolio container">
+      <div className="row justify-content-center">
+        {projects.map((project, index) => (
+          <div key={index} className="col-md-6 col-lg-4 mb-5">
+            <Project
+              title={project.title}
+              imageSrc={project.imageSrc}
+              liveLink={project.liveLink}
+              repoLink={project.repoLink}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
 
 export default Portfolio;
+
+
+
+
+
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const Project = ({ title, imageSrc, liveLink, repoLink }) => {
-  return (
-    <div className="project">
-      <img src={imageSrc} alt={title} />
-      <h3>{title}</h3>
-      <div className="project-links">
-        <a href={liveLink} target="_blank" rel="noopener noreferrer">Live</a>
-        <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub</a>
+    return (
+      <div className="project">
+        <img className="project-image" src={imageSrc} alt={title} />
+        <div className="project-overlay">
+          <div className="project-details">
+            <h3>{title}</h3>
+            <div className="project-links">
+              <a href={liveLink} target="_blank" rel="noopener noreferrer" className="live-link">
+                Live Demo
+              </a>
+              <a href={repoLink} target="_blank" rel="noopener noreferrer" className="repo-link">
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Project;
+    );
+  };
+  
+  export default Project;
+  
