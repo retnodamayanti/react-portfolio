5e57a81c353a25123799381b1bd13f1b0c7096ec
retnodamayanti <mayahere5@gmail.com>
2023-08-09 15:40:29 +0930

    added twitter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-    <div class="social-icons">
+    <footer className='footer text-center card-footer'>
+    <div class="social-icons container">
         <a class="social-icon" href="https://www.linkedin.com/in/retnomaya/"><i class="fab fa-linkedin-in"></i></a>
         <a class="social-icon" href="https://github.com/retnodamayanti"><i class="fab fa-github"></i></a>
+        <a class="social-icon" href="https://twitter.com/itsdamayanti"><i class="fab fa-twitter"></i></a>
     </div>
     </footer>
   );
