8adfcfe774016ccccd1e9ff7d1eff4f7d648e7d3
retnodamayanti <mayahere5@gmail.com>
2023-08-09 15:39:54 +0930

    added validation

--- src/Contact.js
+++ src/Contact.js
@@ -19,13 +19,38 @@ const Contact = () => {
       return;
     }
 
-
     console.log('Form data:', { name, email, message });
   };
 
+  const handleNameBlur = () => {
+    if (!name) {
+      setErrorMessage('All fields are required.');
+    } else {
+      setErrorMessage('');
+    }
+  };
+
+  const handleEmailBlur = () => {
+    if (!email) {
+      setErrorMessage('All fields are required.');
+    } else if (!isValidEmail(email)) {
+      setErrorMessage('Please enter a valid email address.');
+    } else {
+      setErrorMessage('');
+    }
+  };
+
+  const handleMessageBlur = () => {
+    if (!message) {
+      setErrorMessage('All fields are required.');
+    } else {
+      setErrorMessage('');
+    }
+  };
+
   const isValidEmail = (email) => {
-    // 
-    return true;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
   };
 
   return (
@@ -33,15 +58,32 @@ const Contact = () => {
       <form onSubmit={handleFormSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onBlur={handleNameBlur}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={handleEmailBlur}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onBlur={handleMessageBlur}
+          />
         </div>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
         <button type="submit">Submit</button>
